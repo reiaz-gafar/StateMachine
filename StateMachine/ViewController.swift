7301277ae48959f144e9dd11182b5e0b4acea21c
Reiaz Gafar <rgafar@shopkeep.com>
2018-08-03 21:04:43 +0000

    Initial Commit

---
+++ StateMachine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StateMachine
+//
+//  Created by Reiaz Gafar on 8/3/18.
+//  Copyright © 2018 Reiaz Gafar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
